9200631edb440655085a65183e37081c2ee95914
Balázs Kiss <dev@balazskiss.name>
2019-08-21 19:49:35 +0200

    Updated deps

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "MongoProvider",
     dependencies: [
         // MongoDB driver for Fluent
-        .Package(url: "https://github.com/vapor/mongo-driver.git", majorVersion: 2),
+        .package(url: "https://github.com/vapor/mongo-driver.git", from: "2.3.1"),
         // A provider for including Fluent in Vapor applications
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .package(url: "https://github.com/vapor/fluent-provider.git", from: "1.3.0"),
         // A web framework and server for Swift that works on macOS and Ubuntu.
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.4.5")
     ]
 )
