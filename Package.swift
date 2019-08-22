70f4685f2d73b6b65aedb095ca749188f4d69235
Balázs Kiss <dev@balazskiss.name>
2019-08-22 17:51:02 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "MongoProvider",
     dependencies: [
         // MongoDB driver for Fluent
-        //.package(url: "https://github.com/vapor/mongo-driver.git", from: "2.3.1"),
+        .package(url: "https://github.com/vapor/mongo-driver.git", from: "2.3.1"),
         // A provider for including Fluent in Vapor applications
         //.package(url: "https://github.com/vapor/fluent-provider.git", from: "1.3.0"),
         // A web framework and server for Swift that works on macOS and Ubuntu.
